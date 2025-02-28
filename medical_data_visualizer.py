312eabd08a8ce45a38293a61438322c8ad91c264
choward4 <chrishoward1337@gmail.com>
2025-02-28 11:25:51 -0600

    completed draw_cat_plot function

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -8,29 +8,41 @@ df = pd.read_csv('medical_examination.csv')
 
 # 2
 df['overweight'] = df['weight'] / ((df['height'] / 100) ** 2) > 25
-df['overweight'] = df['overweight'].replace([True, False], [1, 0])
+df['overweight'] = df['overweight'].apply(lambda x : 1 if x == True else 0)
 
 # 3 Normalize
-# TODO Test if .apply() is faster
 for col in ('gluc', 'cholesterol'):
     df[col] = df[col].apply(lambda x : 0 if x == 1 else 1)
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
+    values = ['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight']
+    values.sort()
+    
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=values)
 
     # 6
-    df_cat = None
-    
+    totals_0 = []
+    totals_1 = []
 
-    # 7
+    for var in values:
+        var_totals_0 = df_cat[(df_cat['variable'] == var) & (df_cat['cardio'] == 0)]['value'].value_counts().values
+        var_totals_1 = df_cat[(df_cat['variable'] ==  var) & (df_cat['cardio'] == 1)]['value'].value_counts().values
+        totals_0 = np.concatenate((totals_0, var_totals_0))
+        totals_1 = np.concatenate((totals_1, var_totals_1))
+
+    totals = np.concatenate((totals_0, totals_1))
+    
+    df_cat = df_cat.drop_duplicates().sort_values(by=['cardio', 'variable'])
+    df_cat['total'] = totals
+    
+    # 7 TODO
 
 
 
     # 8
-    fig = None
+    fig = sns.catplot(data=df_cat, x='variable', y='total', hue='value', col='cardio', kind='bar')
 
 
     # 9
